0d67ba71e49d2701f692642144eccff08f3412f1
Samuel Ogunbodede <samiwogdev@gmail.com>
2024-06-21 15:04:42 +0100

    Initial commit

---
+++ src/main/java/samiwogdev/com/gretting/GreetingController.java
@@ -0,0 +1,85 @@
+package samiwogdev.com.gretting;
+
+import java.util.concurrent.atomic.AtomicLong;
+
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestMethod;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+public class GreetingController {
+
+	private String history = "";
+	private static final String template = "Hello, %s!";
+	private final AtomicLong counter = new AtomicLong();
+
+//	@RequestMapping("/greeting")
+//	public Greeting greeting(@RequestParam(value = "name", defaultValue = "World") String name) {
+//		return new Greeting(counter.incrementAndGet(), String.format(template, name));
+//	}
+
+//	@RequestMapping("/calculator/add")
+//	public String add(@RequestParam("op1") String op1, @RequestParam("op2") String op2) {
+//		double op1_d = Double.parseDouble(op1);
+//		double op2_d = Double.parseDouble(op2);
+//		String result = op1 + " + " + op2 + " = " + String.valueOf(op1_d + op2_d);
+//		history = history + result + "<br>";
+//		return result;
+//	}
+
+	@RequestMapping({ "/greeting", "/greeting/{name}" })
+	public Greeting greeting(@PathVariable(value = "name", required = false) String name) {
+		 name = (name == null) ? "World" : name;
+		return new Greeting(counter.incrementAndGet(), String.format(template, name));
+	}
+
+	@RequestMapping(value = "/calculator/add/{op1}/{op2}")
+	public String add(@PathVariable("op1") String op1, @PathVariable("op2") String op2) {
+		double op1_d = Double.parseDouble(op1);
+		double op2_d = Double.parseDouble(op2);
+		String result = op1 + " + " + op2 + " = " + String.valueOf(op1_d + op2_d);
+		history = history + result + "<br>";
+		return result;
+	}
+
+	@RequestMapping(value = "/calculator/subtract/{op1}/{op2}")
+	public String subtract(@PathVariable("op1") String op1, @PathVariable("op2") String op2) {
+		double op1_d = Double.parseDouble(op1);
+		double op2_d = Double.parseDouble(op2);
+		String result = op1 + " - " + op2 + " = " + String.valueOf(op1_d - op2_d);
+		history = history + result + "<br>";
+		return result;
+	}
+
+	@RequestMapping(value = "/calculator/multiply/{op1}/{op2}")
+	public String multiply(@PathVariable("op1") String op1, @PathVariable("op2") String op2) {
+		double op1_d = Double.parseDouble(op1);
+		double op2_d = Double.parseDouble(op2);
+		String result = op1 + "  × " + op2 + " = " + String.valueOf(op1_d * op2_d);
+		history = history + result + "<br>";
+		return result;
+	}
+
+	@RequestMapping(value = "/calculator/divide/{op1}/{op2}")
+	public String divide(@PathVariable("op1") String op1, @PathVariable("op2") String op2) {
+		double op1_d = Double.parseDouble(op1);
+		double op2_d = Double.parseDouble(op2);
+        String result = (op2_d == 0) ? op1 + " / " + op2 + " = " + "Infinity" : op1 + " / " + op2 + " = " + String.valueOf(op1_d / op2_d);
+        history = history + result + "<br>";
+        return result;
+	}
+
+	@RequestMapping("/calculator/history")
+	public String history() {
+		return history;
+	}
+
+	@RequestMapping("/calculator/clear")
+	public String clear() {
+		history = "";
+		return "history cleared";
+	}
+
+}
---
+++ src/main/java/samiwogdev/com/gretting/GrettingApplication.java
@@ -0,0 +1,13 @@
+package samiwogdev.com.gretting;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class GrettingApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(GrettingApplication.class, args);
+	}
+
+}
